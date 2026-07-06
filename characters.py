6d9b75b8fb6c7f8c305d84ef523d8637b2bbe7dc
Trinabh Kaushal Bhatt <241409044+bhatttrinabh09-stack@users.noreply.github.com>
2026-07-06 20:04:54 +0530

    Enhance password_generator with comments
    Added comments to clarify the password generation process.

--- characters.py
+++ characters.py
@@ -2,6 +2,7 @@ import random as r
 
 @staticmethod
 def password_generator():
+    #the raw data
     lowercase_alphabets = list("abcdefghijklmnopqrstuvwxyz")
     uppercase_alphabets = list("abcdefghijklmnopqrstuvwxyz".upper())
     numbers = list("1234567890")
@@ -9,13 +10,17 @@ def password_generator():
 
     characters = lowercase_alphabets + numbers + symbols + uppercase_alphabets
 
+    #gets a random size from 8-16 characters
     size = r.randint(8, 16)
-
+    
+    #a blank list
     password_as_list = []
 
+    #appends each character in the list for creating password
     for i in range(size):
         password_as_list.append(r.choice(characters))
 
+    #converts the list into string
     password = "".join(password_as_list)
 
     return password
