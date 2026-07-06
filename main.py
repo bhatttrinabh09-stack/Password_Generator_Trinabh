764c1af4ac9ed609d16658529eba5ed0bb238b19
Trinabh Kaushal Bhatt <241409044+bhatttrinabh09-stack@users.noreply.github.com>
2026-07-06 20:02:55 +0530

    Implement user input for site and username with password storage
    Added functionality to collect site name and username from user and save them along with a generated password to a file.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import characters as c
 
+#gets the site name from user
 site = input("Enter the sitename: ")
+
+#gets the username from user
 username = input("Enter the Username: ")
+
+#generates a random password
 password = c.password_generator()
 
+#appends sitename, username, password in the file to save them all
 with open("password_list.txt", "a") as p:
     p.write(f"Site: {site} \nUsername: {username} \nPassword: {password} \n\n")
